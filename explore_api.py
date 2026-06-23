6ceeedf94b27aca650bea3811eeaeddeded702a0
xinrui33 <xc2731@columbia.edu>
2026-06-23 09:43:48 -0400

    initial commit

--- explore_api.py
+++ explore_api.py
@@ -1,3 +1,19 @@
+# %% Establishing connection to GDELT API and fetching data
 import requests
 
-print("Exploring API calls...")
+url = "https://api.gdeltproject.org/api/v2/doc/doc"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+params = {"query": '("islamic state" OR isis OR daesh)',
+          "mode": "artlist",
+          "maxrecords": 10,
+          "timespan": "1week",
+          "format": "json"}
+resp = requests.get(url, params=params, headers=headers)
+print("Status Code:", resp.status_code)
+data = resp.json()
+print(data.keys())
+# %% Exploring the structure of the returned data
+data["articles"][0].keys()
+data["articles"][0]
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from csuremm!")
+
+
+if __name__ == "__main__":
+    main()
